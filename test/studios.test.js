e327c9cfccbfd38c7d48502a1835c805c978d020
Carmen Perezchica <crperezchica@gmail.com>
2019-01-25 11:12:37 -0800

    created studio schema and create studio test

--- test/studios.test.js
+++ test/studios.test.js
@@ -2,6 +2,8 @@ require ('dotenv').config();
 require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const request = require('supertest');
+const app = require('../lib/app');
+const Studio = require('../lib/models/Studio');
 
 describe('studio app', () => {
   const createStudio = ((name, address) => {
@@ -13,6 +15,34 @@ describe('studio app', () => {
     return mongoose.connection.dropDatabase(() => {
       done();
     });
+  });
+
+  it('can create a new studio', () => {
+    return createStudio('warner', { city: 'LA', state: 'CA', country: 'US' })
+      .then(() => {
+        return request(app)
+          .post('/studios')
+          .send({
+            name: 'warner',
+            address: {
+              city: 'LA',
+              state: 'CA',
+              country: 'US'
+            }
+          })
+          .then(res => {
+            expect(res.body).toEqual({
+              name: expect.any(String),
+              address: {
+                city: 'LA',
+                state: 'CA',
+                country: 'US'
+              },
+              _id: expect.any(String),
+              // __v: 0
+            });
+          });
+      });
   });
 
 });
