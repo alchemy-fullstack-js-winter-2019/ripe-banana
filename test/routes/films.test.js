60cafb442274faff552210b07910a552aba24351
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-25 15:43:24 -0800

    POST /films and test works!

--- test/routes/films.test.js
+++ test/routes/films.test.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 require('../../lib/utils/connect')();
-const Film = require('../../lib/models/Film');
 const request = require('supertest');
 const app = require('../../lib/app');
 const mongoose = require('mongoose');
+// const Film = require('../../lib/models/Film');
 const Studio = require('../../lib/models/Studio');
 const Actor = require('../../lib/models/Actor');
-const { Types } = require('mongoose');
+// const { Types } = require('mongoose');
 
 // const createStudio = (name, address) => {
 //   return request(app)
@@ -18,51 +18,51 @@ const { Types } = require('mongoose');
 //     .then(res => res.body);
 // };
 
-const createStudio = (name = 'BANANA STUDIOS', address = {
-  city: 'portland',
-  state: 'OR',
-  country: 'USA'
-}) => {
-  return Studio.create({ name, address })
-    .then(createdStudio => {
-      return createdStudio;
-    });
-};
+// const createStudio = (name = 'BANANA STUDIOS', address = {
+//   city: 'portland',
+//   state: 'OR',
+//   country: 'USA'
+// }) => {
+//   return Studio.create({ name, address })
+//     .then(createdStudio => {
+//       return createdStudio;
+//     });
+// };
 
-const createActor = (
-  name = 'Banana-jama', 
-  dob = Date.now(),
-  pob = 'Alchemy Code Lab elevator'
-) => {
-  return Actor.create({ name, dob, pob })
-    .then(createdActor => {
-      return createdActor;
-    });
-};
+// const createActor = (
+//   name = 'Banana-jama', 
+//   dob = Date.now(),
+//   pob = 'Alchemy Code Lab elevator'
+// ) => {
+//   return Actor.create({ name, dob, pob })
+//     .then(createdActor => {
+//       return createdActor;
+//     });
+// };
 
-const createFilm = (title, released, role) => {
-  return createStudio('Warner', '123')
-    .then(studio => {
-      return createActor('CariPizzaNana', 1994, 'Florida')
-        .then(actor => {
-          console.log('studio', studio);
-          // console.log('actor', actor);
-          return Film.create({
-            title,
-            studio: studio._id,
-            released: 1203,
-            cast: {
-              role,
-              actor: actor._id
-            }
-          })
-            .then(film => ({
-              ...film,
-              _id: film._id.toString()
-            }));
-        });
-    });
-};
+// const createFilm = (title, released, role) => {
+//   return createStudio('Warner', '123')
+//     .then(studio => {
+//       return createActor('CariPizzaNana', 1994, 'Florida')
+//         .then(actor => {
+//           console.log('studio', studio);
+//           // console.log('actor', actor);
+//           return Film.create({
+//             title,
+//             studio: studio._id,
+//             released: 1203,
+//             cast: {
+//               role,
+//               actor: actor._id
+//             }
+//           })
+//             .then(film => ({
+//               ...film,
+//               _id: film._id.toString()
+//             }));
+//         });
+//     });
+// };
 
 describe('film routes', () => {
   beforeEach(done => {
@@ -74,24 +74,25 @@ describe('film routes', () => {
       .then(studio => {
         return Actor.create({ name: 'actor name', dob: '1888-10-03', pob: 'Wisconsin' })
           .then(actor => {
-            return Film.create({ title: 'Rock of Bananas', studio: studio._id, released: 1885, cast: [{ role: 'Banana Interest', actor: actor._id }] });
+            return request(app)
+              .post('/films')
+              .send({ title: 'Rock of Bananas', studio: studio._id, released: 1885, cast: [{ role: 'Banana Interest', actor: actor._id }] })
+              .then(res => {
+                expect(res.body).toEqual({ __v: 0,
+                  _id: expect.any(String),
+                  cast: 
+                  [{ _id: expect.any(String),
+                    actor: expect.any(String),
+                    role: 'Banana Interest'
+                  }], released: 1885, studio: expect.any(String), title: 'Rock of Bananas' });
+              });
           });
-      })
-      .then(res => {
-        expect(res.toJSON()).toEqual(
-          { __v: 0,
-            _id: expect.any(Types.ObjectId),
-            cast: 
-            [{ _id: expect.any(Types.ObjectId),
-              actor: expect.any(Types.ObjectId),
-              role: 'Banana Interest'
-            }], released: 1885, studio: expect.any(Types.ObjectId), title: 'Rock of Bananas' }
-        );
       });
+  });
+
+  afterAll((done) => {
+    mongoose.disconnect(done);
   });
 });
 
 
-afterAll((done) => {
-  mongoose.disconnect(done);
-});
