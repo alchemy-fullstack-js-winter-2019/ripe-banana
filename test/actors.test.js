cf3d3f48b3a904947a1d98cf91e4901bf3fc36a9
Ivan <i.poblete915@gmail.com>
2019-01-25 14:48:25 -0800

    able to create and Actor

---
+++ test/actors.test.js
@@ -0,0 +1,45 @@
+require ('dotenv').config();
+require('../lib/utils/connect')();
+const mongoose = require('mongoose');
+const request = require('supertest');
+const app = require('../lib/app');
+const Actor = require('../lib/models/Actor');
+
+describe('actor app', () => {
+  const createActor = ((name, dob, pob) => {
+    return Actor.create({ name, dob, pob })
+      .then(actor => ({ ...actor, _id: actor._id.toString() }));
+  });
+  beforeEach(done => {
+    return mongoose.connection.dropDatabase(() => {
+      done();
+    });
+  });
+  afterAll((done) => {
+    mongoose.connection.close(done);
+  });
+
+  it('can create a new actor', () => {
+    return createActor('tom hanks', Date.now(), 'Concord, CA')
+      .then(() => {
+        return request(app)
+          .post('/actors')
+          .send({
+            name: 'tom hanks',
+            dob: Date.now(),
+            pob: 'Concord, CA'
+          })
+          .then(res => {
+            expect(res.body).toEqual({
+              name: 'tom hanks',
+              dob: expect.any(String),
+              pob: 'Concord, CA',
+              _id: expect.any(String),
+              __v: 0
+            });
+          });
+      });
+  });
+
+
+});
