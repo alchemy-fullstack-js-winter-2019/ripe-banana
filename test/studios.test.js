62d847143e14d9e023f3469fd83ddc3971ac4d4b
Dyanna Tolman <dyannast@gmail.com>
2019-01-25 10:36:49 -0800

    finished scaffolding add folders and started on test

---
+++ test/studios.test.js
@@ -0,0 +1,15 @@
+require ('dotenv').config();
+require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const request = require('supertest');
+
+describe('studio app', () => {
+  const createStudio = ((name, address) => {
+  return Studio.create({ name, address })
+  .then(studio => ({ ...studio, _id: studio._id.toString() }));
+  });
+
+  beforeEach
+  }
+}
+
