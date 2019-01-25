bac81a5a40ab1f4a91f20b8b28765b4ffe93fcab
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-25 12:57:42 -0800

    added GET list and test passes

--- test/routes/actors.test.js
+++ test/routes/actors.test.js
@@ -1,16 +1,20 @@
 require('dotenv').config();
 require('../../lib/utils/connect')();
-// const Actor = require('../../lib/models/Actor');
+const Actor = require('../../lib/models/Actor');
 const request = require('supertest');
 const app = require('../../lib/app');
 const mongoose = require('mongoose');
 
-// const createActor = (name, dob, pob) => {
-//   return Actor.create({ name, dob, pob })
-//     .then(createdActor => {
-//       return createdActor;
-//     });
-// };
+const createActor = (
+  name, 
+  dob = Date.now(),
+  pob = 'Alchemy Code Lab elevator'
+) => {
+  return Actor.create({ name, dob, pob })
+    .then(createdActor => {
+      return createdActor;
+    });
+};
 
 describe('actor routes', () => {
 
@@ -33,6 +37,23 @@ describe('actor routes', () => {
             dob: expect.any(String),
             pob: 'Bananaville',
             __v: 0 });
+      });
+  });
+
+  it('gets a list of actors', () => {
+    const actorsToCreate = ['Kevin', 'Ryan', 'Alex'];
+    return Promise.all(actorsToCreate.map(createActor))
+      .then(() => {
+        return request(app)
+          .get('/actors');
+      })
+      .then(res => {
+        expect(res.body).toContainEqual(
+          { _id: expect.any(String), name: 'Kevin' },
+          { _id: expect.any(String), name: 'Alex' },
+          { _id: expect.any(String), name: 'Ryan' } 
+        );
+        expect(res.body).toHaveLength(3);
       });
   });
 
