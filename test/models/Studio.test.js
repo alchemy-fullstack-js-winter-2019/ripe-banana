a5ed81c6c854cce0274877da096f154aa4e09ce1
pickuppatterns <pickup.patterns@gmail.com>
2019-02-02 06:45:43 -0800

    updates package json

---
+++ test/models/Studio.test.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+require('../../lib/utils/connect')();
+const { getErrors } = require('../../lib/middleware/error');
+const mongoose = require('mongoose');
+const { Types } = require('mongoose');
+const Studio = require('../../lib/models/Studio');
+
+describe('Studio model', () => {
+
+  beforeEach(done  => {
+    mongoose.connection.dropDatabase(done);
+  });
+  afterAll(done => mongoose.disconnect(done));
+
+  const studio = new Studio({
+    name: 'A24',
+    address: {
+      city: 'NY',
+      state: 'NY',
+      country: 'US'
+    }
+  });
+  expect(studio.toJSON()).toEqual({
+    _id: expect.any(Types.ObjectId),
+    name: 'A24',
+    address: {
+      city: 'NY',
+      state: 'NY',
+      country: 'US'
+    }
+  });
+  it('requires a name in Schema', () => {
+    const studio = new Studio({
+      address: {
+        city:'NY',
+        state:'NY',
+        country:'US'
+      }
+    });
+    const errors = getErrors(studio.validateSync(), 1);
+    expect(errors.name.kind).toEqual('required');
+  });
+});
+
+
+
