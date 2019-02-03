67657f439962c23ba4ae105411cc364a79e59d83
pickuppatterns <pickup.patterns@gmail.com>
2019-02-03 08:35:23 -0800

    adds film schema tests

--- test/models/Film.test.js
+++ test/models/Film.test.js
@@ -3,21 +3,35 @@ require('../../lib/utils/connect')();
 const mongoose = require('mongoose');
 const { Types } = require('mongoose');
 const Film = require('../../lib/models/Film');
+const Studio = require('../../lib/models/Studio');
 
 describe('Film model', () => {
+  
   beforeEach(done  => {
     mongoose.connection.dropDatabase(done);
   });
+
   afterAll(done => mongoose.disconnect(done));
-it('validates a good model', () => {
-  const film = {
-    title: 'Memento'
-    studio: studio._id,
-    release: 2000,
-    cast: [{
-      role:'Guy Pearce',
-      actor: actor._id
-    }]
-  };
-})
+ 
+  it('validates a good model', () => {
+    
+    const studio = new Studio({
+      name: 'A24',
+      address: {
+        city: 'NY',
+        state: 'NY',
+        country: 'US'
+      }
+    });
+
+    const film = new Film ({
+      title: 'Memento',
+      studio: studio._id,
+      release: 2000,
+      cast: [{
+        role:'Guy Pearce',
+        actor: actor._id
+      }]
+    });
+  });
 });
