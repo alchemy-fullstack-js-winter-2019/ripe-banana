34668544ead27e678c0d3dfda7ab0cb5daebed00
Marcy Silverman <marcysilve225@gmail.com>
2019-02-02 18:26:04 -0800

    set up files and models

---
+++ server.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+require('./lib/utils/connect')();
+
+const app = require('./lib/app');
+const PORT = process.env.PORT || 7890; 
+
+app.listen(PORT, (a) => {
+    // eslint-disable-next-line no-console
+    console.log('running', PORT);
+});
