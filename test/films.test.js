8ecac2d5a618acf4e8bb8c09fe9ec35318071a36
Marcy Silverman <marcysilve225@gmail.com>
2019-02-02 20:03:04 -0800

    reviews post, gets, delet, and put are working

---
+++ test/films.test.js
@@ -0,0 +1,126 @@
+require('dotenv').config();
+require('../lib/utils/connect')();
+const request = require('supertest');
+const app = require('../lib/app');
+const mongoose = require('mongoose');
+
+const createFilm = (title = 'Saw', released = 2003) => {
+    return createStudio()
+        .then(createStudio => {
+            return request(app) 
+                .post('/films')
+                .send({
+                    title,
+                    released,
+                    studio: createStudio._id
+                })
+                .then(res => res.body);
+        });
+};
+
+const createStudio = (name = 'Universal', address = { city: 'Los Angeles', state: 'Cali', country: 'USA' }) => {
+    return request(app)
+        .post('/studios')
+        .send({
+            name,
+            address
+        })
+        .then(res => res.body);
+};
+describe('films', () => {
+    beforeEach(done => {
+        return mongoose.connection.dropDatabase(() => {
+            done();
+        });
+    });
+    afterAll(done => {
+        mongoose.connection.close(done);
+    });
+    it('creates an film', () => {
+        return createStudio('Universal')
+            .then(createStudio => {
+                return request(app)
+                    .post('/films')
+                    .send({
+                        title: 'Saw',
+                        released: '2003',
+                        studio: createStudio._id
+                    })
+                    .then(res => {
+                        expect(res.body).toEqual({
+                            title: 'Saw',
+                            released: 2003,
+                            studio: expect.any(String),
+                            cast: [],
+                            _id: expect.any(String),
+                            __v: 0
+                        });
+                    });
+            });
+    });
+    // it('gets all the films', () => {
+    //     const films = ['actor1', 'actor2', 'actor3'];
+    //     return Promise.all(films.map(createActor))
+    //         .then(() => {
+    //             return request(app)
+    //                 .get('/films');
+    //         })
+    //         .then(({ body }) => {
+    //             expect(body).toHaveLength(3);
+    //         });
+    // });
+//     it('gets an actor by id', () => {
+//         return createActor('chris')
+//             .then(actor => {
+//                 return request(app)
+//                     .get(`/films/${actor._id}`)
+//                     .then(res => {
+//                         expect(res.body).toEqual({
+//                             name: 'chris',
+//                             _id: expect.any(String),
+//                             dob: '1980-04-20T08:00:00.000Z',
+//                             pob: 'Paris, France',
+//                             __v: 0
+//                         });
+//                     });
+//             });
+//         // const movie = { 
+//         //     title: 'James Bond', 
+//         //     studio: mongoose.Types.ObjectId(),
+//         //     released: 1969,
+//         //     cast: [{
+//         //         part: 'Lead',
+//         //         actor: helen._id
+//         //     }]
+//         // };
+//     });
+//     it('gets an actor by id and updates', () => {
+//         return createActor('Robert')
+//             .then(actor => {
+//                 actor.name = 'Kate';
+//                 return request(app)
+//                     .put(`/films/${actor._id}`)
+//                     .send(actor);
+//             })
+//             .then(res => {
+//                 expect(res.body).toEqual({
+//                     name: 'Kate',
+//                     _id: expect.any(String),
+//                     dob: '1980-04-20T08:00:00.000Z',
+//                     pob: 'Paris, France',
+//                     __v: 0
+//                 });
+//             });
+//     });
+//     it('deletes an actor', () => {
+//         return createActor('Brad')
+//             .then(actor => {
+//                 const id = actor._id;
+//                 return request(app)
+//                     .delete(`/films/${id}`);
+//             })
+//             .then(res => {
+//                 expect(res.body).toEqual({ deleted: 1 });
+//             });
+//     });
+});
---
+++ test/reviewers.test.js
@@ -0,0 +1,85 @@
+require('dotenv').config();
+require('../lib/utils/connect')();
+const request = require('supertest');
+const app = require('../lib/app');
+const mongoose = require('mongoose');
+
+const createReviewer = (name) => {
+    return request(app)
+        .post('/reviewers')
+        .send({ 
+            name: name,
+            company: 'company'
+        })
+        .then(res => res.body);
+};
+
+describe('reviewers', () => {
+    beforeEach(done => {
+        return mongoose.connection.dropDatabase(() => {
+            done();
+        });
+    });
+
+    it('creates a reviewer', () => {
+        return request(app)
+            .post('/reviewers')
+            .send({
+                name: 'John',
+                company: 'company'
+            })
+            .then(res => {
+                expect(res.body).toEqual({
+                    name: 'John',
+                    company: 'company',
+                    _id: expect.any(String),
+                    __v: 0
+                });
+            });
+    });
+});
+
+it('gets all reviewers', () => {
+    const names = ['john', 'muffin', 'person'];
+    return Promise.all(names.map(createReviewer))
+        .then(() => {
+            return request(app)
+                .get('/reviewers');
+        })
+        .then(({ body }) => {
+            expect(body).toHaveLength(4);
+        });
+});
+
+it('gets a reviewer by id', () => {
+    return createReviewer('poppy')
+        .then(createdReviewer => {
+            return request(app) 
+                .get(`/reviewers/${createdReviewer._id}`)
+                .then(res => {
+                    expect(res.body).toEqual({
+                        name: 'poppy',
+                        company: 'company',
+                        _id: expect.any(String)
+                    });
+                });
+        });
+});
+
+it('updates a reviewer and updates', () => {
+    return createReviewer('puppy')
+        .then(createdReviewer => {
+            createdReviewer.name = 'scott';
+            return request(app)
+                .put(`/reviewers/${createdReviewer._id}`)
+                .send(createdReviewer);
+        })
+        .then(res => {
+            expect(res.body).toEqual({
+                name: 'scott',
+                company: 'company',
+                _id: expect.any(String),
+                __v: 0
+            });
+        });
+});
